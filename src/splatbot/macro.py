6c667c2db49293ce77173507fdf27059470027cf
necusjz <necusjz@gmail.com>
2023-12-09 22:50:22 +0800

    feat: enable generate macro (simple bounce)

---
+++ src/splatbot/macro.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import Image
+
+
+def generate_macro(image_path):
+    with Image.open(image_path) as img:
+        if img.size != (320, 120):
+            raise ValueError("Image size must be 320 by 120 pixels.")
+
+        img = img.convert("1")
+        img = np.array(img.getdata()).reshape(120, 320) / 255
+        img = 1 - img.astype(int)
+
+    m, n = img.shape
+    commands = []
+
+    for i in range(m):
+        if i != 0:
+            commands.append("d")
+
+        if not i & 1:
+            for j in range(n):
+                if img[i][j] == 1:
+                    commands.append("a")
+
+                if j != n - 1:
+                    if commands and commands[-1] == "r":
+                        commands += ["d", "u", "r"]
+                    else:
+                        commands.append("r")
+        else:
+            for j in range(n - 1, -1, -1):
+                if img[i][j] == 1:
+                    commands.append("a")
+
+                if j != 0:
+                    if commands[-1] == "l":
+                        commands += ["u", "d", "l"]
+                    else:
+                        commands.append("l")
+
+    commands.append("b")  # save and quit
+
+    with open("sequence", "w", encoding="utf-8") as fp:
+        fp.write("\n".join(commands))
