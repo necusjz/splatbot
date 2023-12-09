8d932f2b908b973f2fd698de0bdf216b17dbee37
necusjz <necusjz@gmail.com>
2023-12-10 00:49:59 +0800

    feat: add time stats for plotting

--- src/splatbot/start.py
+++ src/splatbot/start.py
@@ -1,16 +1,18 @@
+import time
+
 from .controller import Button
 from .controller import Controller
 
 
 command_map = {
-    "u": Button.DPAD_UP,
-    "d": Button.DPAD_DOWN,
-    "l": Button.DPAD_LEFT,
-    "r": Button.DPAD_RIGHT,
     "a": Button.A,
     "b": Button.B,
     "x": Button.X,
-    "y": Button.Y
+    "y": Button.Y,
+    "u": Button.DPAD_UP,
+    "d": Button.DPAD_DOWN,
+    "l": Button.DPAD_LEFT,
+    "r": Button.DPAD_RIGHT
 }
 
 
@@ -20,9 +22,15 @@ def start_plotting(macro_path):
 
     print("Navigate to Change Grip/Order menu on your Nintendo Switch.")
 
-    with Controller(press_ms=700, delay_ms=7000) as ctl:
+    with Controller(press_ms=600, delay_ms=7000) as ctl:
         print("Connected!")
         input("Press <enter> key to start plotting...")
 
         buttons = [command_map[cmd.strip()] for cmd in commands]
+
+        start = time.time()
         ctl.macro(buttons)
+        end = time.time()
+        elapsed = time.strftime("%H:%M:%S", time.gmtime(end - start))
+
+        print(f"`splatbot` took {elapsed} to complete your artwork.")
