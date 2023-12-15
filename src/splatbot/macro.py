565f3a48bbfe40691d50cc75b1ad86ad064fd699
necusjz <necusjz@gmail.com>
2023-12-15 16:13:34 +0800

    refactor: extract pathing from generate_macro

--- src/splatbot/macro.py
+++ src/splatbot/macro.py
@@ -2,16 +2,8 @@ import numpy as np
 from PIL import Image
 
 
-def generate_macro(image_path):
-    with Image.open(image_path) as img:
-        if img.size != (320, 120):
-            raise ValueError("Image size must be 320 by 120 pixels.")
-
-        img = img.convert("1")
-        img = np.array(img.getdata()).reshape(120, 320) / 255
-        img = 1 - img.astype(int)
-
-    m, n = img.shape
+def pathing(matrix):
+    m, n = matrix.shape
     commands = []
 
     for i in range(m):
@@ -20,7 +12,7 @@ def generate_macro(image_path):
 
         if not i & 1:
             for j in range(n):
-                if img[i][j] == 1:
+                if matrix[i][j] == 1:
                     commands.append("a")
 
                 if j != n - 1:
@@ -30,7 +22,7 @@ def generate_macro(image_path):
                         commands.append("r")
         else:
             for j in range(n - 1, -1, -1):
-                if img[i][j] == 1:
+                if matrix[i][j] == 1:
                     commands.append("a")
 
                 if j != 0:
@@ -41,13 +33,20 @@ def generate_macro(image_path):
 
     commands.append("b")  # save and quit
 
-    print("Benchmark: Pathing algorithm is ", end="")
-    prev = 120 * 320 + np.count_nonzero(img)
-    curr = len(commands)
-    if curr < prev:
-        print(f"{prev / curr:.2f}x faster ({curr} vs. {prev}).")
-    else:
-        print(f"{curr / prev:.2f}x slower ({curr} vs. {prev}).")
+    return commands
+
+
+def generate_macro(image_path):
+    with Image.open(image_path) as img:
+        if img.size != (320, 120):
+            raise ValueError("Image size must be 320 by 120 pixels.")
+
+        img = img.convert("1")
+
+        matrix = np.array(img.getdata()).reshape(120, 320) / 255
+        matrix = 1 - matrix.astype(int)
+
+    commands = pathing(matrix)
 
     with open("sequence", "w", encoding="utf-8") as fp:
         fp.write("\n".join(commands))
