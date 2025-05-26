4047ccd0eb545ad1125e32eff4e252d9c4186ad1
necusjz <necusjz@gmail.com>
2025-05-26 15:06:23 +1000

    chore: set executable permission

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,7 @@ TAG=$(python -c 'from src.splatbot import __version__; print("v" + __version__)'
 read -p "Creating new release for $TAG. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
-    python scripts/prepare_changelog.py
+    ./scripts/prepare_changelog.py
     git add -A
     git commit -m "build: release $TAG" || true && git push
     echo "Creating new git tag $TAG."
